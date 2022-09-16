d5688f8e28f337051a26325b09dd6c909e942b68
rahulp229 <rahulp229@gmail.com>
2022-09-16 16:54:25 +0530

    added new program

---
+++ 15_find_equvi_distanc_bet_numbers/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Hello, playground")
+	//arr := []float64{6, 7, 9, 11}
+	result := []float64{}
+	arr := []float64{6, 8, 10, 14, 18, 14, 43}
+	for key, val := range arr {
+		if key < len(arr)-1 {
+
+			diff := math.Abs(val - arr[key+1])
+			//mid = math.Abs(diff)
+			mid := diff / 2
+
+			fmt.Println("diff ", diff)
+
+			if diff > 2 {
+				if val < arr[key+1] {
+					result = append(result, val+mid)
+				} else {
+					result = append(result, val-mid)
+				}
+			}
+		}
+	}
+
+	fmt.Println("result ", result)
+
+}
